4cf01601d48b87d98e6e17edd02e6ca3fce018db
gengbingbing <992899491@qq.com>
2018-09-07 23:41:15 +0800

    配置

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -28,6 +28,7 @@ module.exports = {
     alias: {
       'vue$': 'vue/dist/vue.esm.js',
       '@': resolve('src'),
+      'vendor': path.resolve(__dirname, '../src/vendor'),
     }
   },
   module: {
