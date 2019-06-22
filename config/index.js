7fcac67637f51fd2d2b7371d14404437406444ad
gengbingbing <992899491@qq.com>
2019-06-22 08:22:10 +0800

    修改配置文件，打包输出路径改为vueTool

--- config/index.js
+++ config/index.js
@@ -46,10 +46,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../../dist/index.html'),
+    index: path.resolve(__dirname, '../vueTool/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../../dist'),
+    assetsRoot: path.resolve(__dirname, '../vueTool'),
     assetsSubDirectory: 'static',
     assetsPublicPath: './',
 
